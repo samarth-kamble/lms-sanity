c39da6185d46c0ebf93709ab504593f23020a808
Samarth Kamble <kamblesamarth04@gmail.com>
2025-04-22 10:52:48 +0530

    Final Push

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Roboto_Condensed, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { draftMode } from "next/headers";
+import { VisualEditing } from "next-sanity";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
+
+const geistSans = Roboto_Condensed({
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Courselly - A Next.js 13 LMS",
+  description: "A Next.js 13 LMS built with Sanity and Stripe for course creation",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.className} antialiased`}
+      >
+        {(await draftMode()).isEnabled && (
+          <>
+            <DisableDraftMode />
+            <VisualEditing />
+          </>
+        )}
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
